b2129599d8aaebddfc0b71496083fbdb0c103c1d
Kshitij Tyagi <84868765+KT-TK@users.noreply.github.com>
2023-05-23 13:27:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.ceil((Math.random() * 6));
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+var randomNumber2 = Math.ceil((Math.random() * 6));
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+if (randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+else if (randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "🚩Draw🚩";
+}
